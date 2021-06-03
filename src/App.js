d4f5e90bc82ad9cf760ec7db4b54a1634f6a343c
Naman <namansingh269@gmail.com>
2021-06-03 21:38:25 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Login from './components/Login/Login'
 import Profile from './components/Profile/Profile';
 import SignUp from './components/SignUp/SignUp';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
-import Branches from './components/Branches/Branches'
+import Branches from './components/Branches/Branches';
+import {Route,Link} from 'react-router-dom';
+
 function App() {
   const [posts,setPosts]=useState([
     {
@@ -65,10 +67,15 @@ function App() {
 
    </div>
     }
-     <NavBar/>
-<Branches/>       
+     <NavBar/>    
+    <Route exact path="/" component={Branches}/>
+    <Route exact path="/login" component={Login}/>
+    <Route  exact path="/signup" component={SignUp}/>
+    <Route exact path="/ask" component={Ask}/>
+    <Route exact path="/profile" component={Profile}/>
     </div>
   );
 }
 
 export default App;
+
